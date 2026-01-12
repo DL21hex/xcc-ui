567166e2500dfe821fa285e6268c9db18e8b9492
DL21hex <diego.llinas@gmail.com>
2026-01-11 19:58:01 -0500

    Actualizando plantilla...

--- src/app.tsx
+++ src/app.tsx
@@ -1,20 +1,19 @@
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
-import Nav from "~/components/Nav";
+//import Nav from "~/components/Nav";
 import "./app.css";
 
-export default function App() {
-  return (
-    <Router
-      root={props => (
-        <>
-          <Nav />
-          <Suspense>{props.children}</Suspense>
-        </>
-      )}
-    >
-      <FileRoutes />
-    </Router>
-  );
+export default function App()
+{
+	return (
+		<Router
+			root={props => (
+			<>
+			<Suspense>{props.children}</Suspense>
+			</>
+		)}>
+	<FileRoutes />
+	</Router>
+);
 }
